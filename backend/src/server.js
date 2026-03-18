a2998b84398e5bd46781660061faa4bc8be3eca6
Nivya Alikanti <nivyaalikanti@gmail.com>
2026-03-18 21:55:52 +0530

    Delete backend directory

--- backend/src/server.js
+++
@@ -1,8 +0,0 @@
-import app from './app.js';
-import { config } from './config.js';
-import { startDailyAlerts } from './services/scheduler.js';
-
-app.listen(config.port, () => {
-  console.log(`SunSafe API listening on :${config.port}`);
-  startDailyAlerts();
-});
