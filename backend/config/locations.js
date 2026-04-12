2517d95a906dc0469b228261721c462f7e751494
Alikanti Nivya <nivyaalikanti@gmail.com>
2026-04-12 20:44:58 +0530

    Create lat, long data for 5 cities

---
+++ backend/config/locations.js
@@ -0,0 +1,7 @@
+module.exports = [
+  { name: "Delhi", lat: 28.6139, lng: 77.2090 },
+  { name: "Mumbai", lat: 19.0760, lng: 72.8777 },
+  { name: "Hyderabad", lat: 17.3850, lng: 78.4867 },
+  { name: "Chennai", lat: 13.0827, lng: 80.2707 },
+  { name: "Kolkata", lat: 22.5726, lng: 88.3639 },
+];
